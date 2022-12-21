9596ae96306f60394c2070f5a03ff6ef718f42ee
dyng <65110016+dyngx@users.noreply.github.com>
2022-12-22 01:48:24 +0700

    Update real_time.py

--- real_time.py
+++ real_time.py
@@ -1,19 +1,16 @@
 import os
 import time
-
 import cv2
 import mediapipe as mp
 import numpy as np
-import tensorflow as tf
 from keras.models import load_model
 from keras.callbacks import TensorBoard
 from keras.layers import LSTM, Dense
 from keras.models import Sequential
 from keras.utils import to_categorical
-from matplotlib import pyplot as plt
-from sklearn.metrics import accuracy_score, multilabel_confusion_matrix
 from sklearn.model_selection import train_test_split
 from scipy import stats
+
 
 mp_holistic = mp.solutions.holistic  # Holistic model
 mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
@@ -27,11 +24,6 @@ def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) #RGB -> BGR
     return image, results
 
-def draw_landmarks(image, result):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) #draw face connections; 
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) #draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
 def draw_styled_landmarks(image, result):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
@@ -59,23 +51,18 @@ def extract_keypoints(results):
 
 DATA_PATH = os.path.join('MP_Data')
 # list of actions   
-actions = np.array(['hello','thanks','salanghaeyo~','sad','family'])
+actions = np.array(['hello','thanks'])
 # 30 videos of data
-no_sequences = 30
+no_sequences = 10
 # 30 frames
-sequence_length = 30
+sequence_length = 60
 
 
 model=load_model('actions.h5')
 
 
 colors = [(245,117,16), (117,245,16), (16,117,245)]
-def prob_viz(res, actions, input_frame, colors):
-    output_frame = input_frame.copy()
-    for num, prob in enumerate(res):
-        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
-        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-    return output_frame
+
 
 
 
@@ -90,7 +77,7 @@ threshold = 0.5
 
 cap = cv2.VideoCapture(0)
 # Set mediapipe model 
-with mp_holistic.Holistic(min_detection_confidence=0.3, min_tracking_confidence=0.3) as holistic:
+with mp_holistic.Holistic(min_detection_confidence=0.4, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
 
         # Read feed
@@ -106,9 +93,9 @@ with mp_holistic.Holistic(min_detection_confidence=0.3, min_tracking_confidence=
         # 2. Prediction logic
         keypoints = extract_keypoints(results)
         sequence.append(keypoints)
-        sequence = sequence[-30:]
+        sequence = sequence[-60:]
         
-        if len(sequence) == 30:
+        if len(sequence) == 60:
             res = model.predict(np.expand_dims(sequence, axis=0))[0]
             print(actions[np.argmax(res)])
             predictions.append(np.argmax(res))
@@ -133,12 +120,12 @@ with mp_holistic.Holistic(min_detection_confidence=0.3, min_tracking_confidence=
         cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
         cv2.putText(image, ' '.join(sentence), (3,30), 
                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        
+
         # Show to screen
         cv2.imshow('OpenCV Feed', image)
-
         # Break gracefully
         if cv2.waitKey(10) & 0xFF == ord('q'):
+
             break
     cap.release()
     cv2.destroyAllWindows()
